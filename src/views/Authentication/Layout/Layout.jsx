9884902e8f56d9b59d28ac0a4f4c3793d29a4e6e
abass <haybeecodes@gmail.com>
2023-02-26 03:10:57 +0100

    [refactor] remove codes not required from Layout.jsx in Authentication folder

--- src/views/Authentication/Layout/Layout.jsx
+++ src/views/Authentication/Layout/Layout.jsx
@@ -6,8 +6,8 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-    <section className="w-screen h-screen  relative grid md:flex flex-wrap grid-cols-6  bg-primary-02 overflow-hidden">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#1C0000]/70"></div>
+    <section className="w-screen h-screen  relative grid md:flex flex-wrap grid-cols-6  overflow-hidden">
+      {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#1C0000]/70"></div> */}
      
       <div className="flex flex-col gap-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 
