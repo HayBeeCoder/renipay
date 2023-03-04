7b8fc9ff540112599bd55ad2697ec9aaf9370316
HayBeeCoder <haybeecodes@gmail.com>
2023-03-04 12:20:35 +0100

    [feature] add icons to react app

--- src/views/Authentication/Layout/Layout.jsx
+++ src/views/Authentication/Layout/Layout.jsx
@@ -8,8 +8,8 @@ const Layout = ({ children }) => {
   return (
     <section className="w-screen h-screen  relative grid md:flex flex-wrap grid-cols-6  overflow-hidden">
       <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between overflow-hidden">
-        <p className="md:text-[#F5F5F5] text-faint font-black md:text-[100px] text-[120px] text-left  opacity-5 md:opacity-70">Renipay</p>
-        <p className="md:text-[#F5F5F5] text-faint font-black md:text-[100px] text-[120px] text-right -ml-12 md:opacity-50 opacity-5">Renipay</p>
+        <p className="text-[#F5F5F5]  font-black md:text-[100px] text-[120px] text-left  opacity-90">Renipay</p>
+        <p className="text-[#F5F5F5]  font-black md:text-[100px] text-[120px] text-right -ml-12 opacity-90">Renipay</p>
       </div>
      
       <div className="flex flex-col gap-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
