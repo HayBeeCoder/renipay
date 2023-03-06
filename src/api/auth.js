ac0cfdad46e0885d50a44475133996a07aed4a33
HayBeeCoder <haybeecodes@gmail.com>
2023-03-06 05:50:15 +0100

    [update]

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,5 +2,6 @@ import api from "./api";
 const createUser = (newUser) =>
   api.post("/register", newUser).then((res) => res);
 const logUserIn = (newUser) => api.post("/login", newUser).then((res) => res);
+const getUser = (token) => api.post("/auth", { token }).then((res) => res);
 
-export { createUser, logUserIn };
+export { createUser, logUserIn, getUser };
